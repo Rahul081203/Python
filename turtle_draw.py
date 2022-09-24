f8e36abd764efac658faa35f046bad9f4ac623d7
Rahul <rahulthor.081203@gmail.com>
2022-09-24 11:45:43 +0530

    Made some additions like mirror image drawing and certain commands

--- turtle_draw.py
+++ turtle_draw.py
@@ -11,8 +11,39 @@ b=turtle.Turtle()
 #Initiating a turtle object 'c' for 
 tracker=turtle.Turtle()
 
+#Initiating mirror turtle
+mirror=turtle.Turtle()
+mirror.hideturtle()
+
+#Initiating mirror image turtle
+image=turtle.Turtle()
+image.penup()
+image.hideturtle()
+
+#Initiating instructions turtle
+ins=turtle.Turtle()
+ins.hideturtle()
+ins.penup()
+ins.setpos(-382,296)
+ins.seth(0)
+ins.pendown()
+
+# Instruction Box
+for x in range(2):
+    ins.fd(200)
+    ins.rt(90)
+    ins.fd(160)
+    ins.rt(90)
+ins.penup()
+ins.setpos(-375,160)
+ins.pendown()
+ins.write("Pen color:'q'/'e'  BG color:'b'\nFill Color:'v'  Pen-up:'ctrl'\nPen-down:'tab' Movement:'Arrows'\nShow Mirror:'m'  Hide Mirror:'h'\nStart mirror_image:'i' \nStop mirror_image:'o'\nReset:'c' Begin_fill:'f'\nEnd_fill:'d'  Clear drawing:'ctrl+z'\nClear mirror_image:'n'")
 #Setting turtle speed to max
 a.speed(0)
+image.speed(0)
+b.speed(0)
+mirror.speed(0)
+tracker.speed(0)
 
 #Creating a list of colours to traverse through for changing colours
 colours=["red","blue","green","yellow","violet","white","purple","brown","orange","gray","olive","maroon","magenta","teal","black","navy blue","cyan","gold","pink"]
@@ -49,16 +80,13 @@ while True:
     current_fill_color=colours[col]
     turtle.bgcolor(current_bg_color)
     a.color(current_colour,current_fill_color)
-    
+    image.color(current_colour,current_fill_color)
+
     #Setting turtle position as previous position and showing it after every iteration as clear can be pressed by user which deletes the turtle
     pos=a.pos()
-    #
 
     #Clearing the tracker info at each iteration to replace it with new one
     tracker.clear()
-
-    #Clearing the screen and assigning ops to various key-presses
-#    turtle.clearscreen()
     
     # MOVE FORWARD
     if keyboard.is_pressed("up"):
@@ -67,6 +95,7 @@ while True:
         elif keyboard.is_pressed("left"): # MAKE AN ARC TO THE LEFT
             a.lt(3)
         a.fd(5)
+        image.setpos(-(tuple(a.pos())[0]),tuple(a.pos())[1])
         continue;
 
     # MOVE BACKWARDS
@@ -76,6 +105,7 @@ while True:
         elif keyboard.is_pressed("left"): # MAKE AN ARC TO THE LEFT
             a.lt(3)
         a.fd(-5)
+        image.setpos(-(tuple(a.pos())[0]),tuple(a.pos())[1])
         continue;
 
     # ROTATE TO THE RIGHT
@@ -106,21 +136,29 @@ while True:
     
     # PEN-UP
     elif keyboard.is_pressed("ctrl"):
-        a.penup()
+        if keyboard.is_pressed('z'):
+            a.clear()
+        else:
+            image.penup()
+            a.penup()
         continue;
 
     # PEN-DOWN
     elif keyboard.is_pressed("tab"):
         a.pendown()
+        image.pendown()
         continue;
 
     
     # RESET SCREEN
     elif keyboard.is_pressed("c"):
-        turtle.clearscreen()
-        a=turtle.Turtle()
+        a.clear()
+        image.clear()
+        mirror.clear()
         a.penup()
-        a.setpos(pos)
+        image.penup()
+        a.setpos(0,0)
+        image.setpos(0,0)
 
     # SWITCH BACKGROUND COLOR
     elif keyboard.is_pressed("b"):
@@ -136,10 +174,12 @@ while True:
     # BEGIN-FILLING COLOR
     elif keyboard.is_pressed("f"):
         a.begin_fill()
+        image.begin_fill()
 
     # END-FILLING COLOR
     elif keyboard.is_pressed("d"):
         a.end_fill()
+        image.end_fill()
 
 
     # SWITCH FILL COLORS
@@ -149,8 +189,41 @@ while True:
         else:
             col=col+1
 
+    # SHOW MIRROR
+    elif keyboard.is_pressed("m"):
+        
+        mirror.penup()
+        mirror.setpos(0,-1000)
+        mirror.seth(90)
+        mirror.speed(0)
+        mirror.pendown()
+        
+        # DRAW MIRROR
+        while tuple(mirror.pos())[1]<=tuple((0.00,1000.00))[1]:
+            mirror.fd(6)
+            mirror.penup()
+            mirror.fd(4)
+            mirror.pendown()
+        mirror.penup()
+    
+    # REMOVE MIRROR
+    elif keyboard.is_pressed("h"):
+        mirror.clear()
+
+    # START MIRROR IMAGE
+    elif keyboard.is_pressed("i"):
+        image.pendown()
+        image.hideturtle()
+    
+    # STOP MIRROR IMAGE
+    elif keyboard.is_pressed("o"):
+        image.penup()
+
+    # CLEAR MIRROR IMAGE
+    elif keyboard.is_pressed('n'):
+        image.clear()
     else:
         pass
 
     #WRITE ALL CHANGES TO TRACKER
-    tracker.write("Current Pen Color: "+current_colour+"\nCurrent BG Color: "+current_bg_color+"\nCurrent Fill Color: "+current_fill_color+"\nPosition: "+str(pos)+"\nHeading: "+str(a.heading()),font=('Arial',9,'normal'))
+    tracker.write("Current Pen Color: "+current_colour+"\nCurrent BG Color: "+current_bg_color+"\nCurrent Fill Color: "+current_fill_color+"\nPosition: "+str(pos)+"\nHeading: "+str(a.heading())+"\nMirror position: "+str(image.pos()),font=('Arial',9,'normal'))
