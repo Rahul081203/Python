10c4f12e030f6ff57838d1b988746a0612849723
Rahul <rahulthor.081203@gmail.com>
2022-10-08 16:15:07 +0530

    Updated pathFinder

--- PyGame/pathFinder/pathFinder.py
+++ PyGame/pathFinder/pathFinder.py
@@ -30,8 +30,50 @@ class Node:
         self.neighbors = []
         self.width = width
         self.total_rows = total_rows
+
+    def get_pos(self):
+        return self.row,self.col
+
+    def is_closed(self):
+        return self.color == RED
+    def is_open(self):
+        return self.color == GREEN
+    def is_barrier(self):
+        return self.color == BLACK
     
-        
+    def is_start(self):
+        return self.color == ORANGE
+    def is_end(self):
+        return self.color == PURPLE
+    def reset(self):
+        return self.color == WHITE
+    
+    def make_closed(self):
+        return self.color == RED
+
+    def make_open(self):
+        return self.color == GREEN
+    def make_barrier(self):
+        return self.color == BLACK
+    def make_end(self):
+        return self.color == TURQUOISE
+    def make_path(self):
+        return self.color == PURPLE
+    
+    def draw(self,win):
+        pygame.draw.rect(win,self.color,(self.x,self.y,self.width,self.width))
+    
+    def update_neighbors(self,grid):
+        pass
+
+    def __lt__(self,other):
+        return False
+
+# MANHATTTAN DISTANCE : distance for the quickest L-shaped path from current node to destination
+def h(p1,p2):
+    x1,y1=p1
+    x2,y2=p2
+    return abs(x1-x2)+abs(y1-y2)
 
 
 pygame.display.set_caption("A* PathFinder")
