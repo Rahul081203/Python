cb5ef603f24d24b679db206f6741e06021ff1bfc
Rahul <rahulthor.081203@gmail.com>
2022-10-08 10:46:59 +0530

    Added Health Bar

--- PyGame/first game.py
+++ PyGame/first game.py
@@ -1,6 +1,13 @@
 from pickle import TRUE
 import pygame
 import os
+from pygame.locals import *
+pygame.init()
+
+
+
+import os
+os.getcwd()
 WIDTH,HEIGHT=900,500
 
 WHITE=(255,255,255)
@@ -22,8 +29,8 @@ SPACESHIP_HEIGHT=60
 BLUE_HIT = pygame.USEREVENT + 1
 RED_HIT = pygame.USEREVENT + 2
 
-BLUE_SPACESHIP_IMAGE=pygame.image.load("./Assets/blue.png")
-RED_SPACESHIP_IMAGE=pygame.image.load("./Assets/red.png")
+BLUE_SPACESHIP_IMAGE=pygame.image.load(os.path.join('Assets','blue.png'))
+RED_SPACESHIP_IMAGE=pygame.image.load(os.path.join('Assets','red.png'))
 BLUE_SPACESHIP=pygame.transform.rotate(pygame.transform.scale(BLUE_SPACESHIP_IMAGE,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),270)
 RED_SPACESHIP=pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),90)
 
@@ -31,14 +38,24 @@ SPACE = pygame.transform.scale(pygame.image.load("./Assets/space.png"),(WIDTH,HE
 
 BORDER=pygame.Rect((WIDTH/2) - 5,0,10,HEIGHT)
 
+HEALTH_FONT = pygame.font.SysFont('comicsans',40)
+WINNER_FONT = pygame.font.SysFont('algerian',100)
+
+
 #What we want to draw
-def draw_window(blue,red,red_bullets,blue_bullets):
+def draw_window(blue,red,red_bullets,blue_bullets,blue_health,red_health):
     WIN.blit(SPACE,(0,0))
     pygame.draw.rect(WIN,BLACK,BORDER)
+
+    red_health_text=HEALTH_FONT.render("Health: "+str(red_health),1,WHITE)
+    blue_health_text=HEALTH_FONT.render("Health: "+str(blue_health),1,WHITE)
+
     WIN.blit(BLUE_SPACESHIP,(blue.x,blue.y))#to load surfaces to your pygame window you have to use blit function
     #In pygame the origin lies on the top left corner of the window 
     WIN.blit(RED_SPACESHIP,(red.x,red.y))
 
+    WIN.blit(red_health_text,(WIDTH - red_health_text.get_width()-10,10))
+    WIN.blit(blue_health_text,(10,10))
     for bullet in red_bullets:
         pygame.draw.rect(WIN,RED,bullet)
     for bullet in blue_bullets:
@@ -81,6 +98,14 @@ def bullets_control(blue_bullets, red_bullets, blue, red):
             red_bullets.remove(bullet)
         elif bullet.x < 0:
             red_bullets.remove(bullet)
+
+def show_winner(text):
+    winner_text=WINNER_FONT.render(text,1,WHITE)
+    WIN.blit(winner_text,(WIDTH/2 - winner_text.get_width()/2, HEIGHT/2-winner_text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(10000)
+
+
 #Infinite Loop
 def main():
     red=pygame.Rect(700,300,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)
@@ -97,6 +122,7 @@ def main():
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 run=False
+                pygame.quit()
             if event.type==pygame.KEYDOWN:
                 if event.key == pygame.K_LCTRL and len(blue_bullets) < MAX_BULLETS:
                     bullet=pygame.Rect(blue.x+blue.width,blue.y+ blue.height/2,10,5)
@@ -108,11 +134,14 @@ def main():
                 red_health-=1
             if event.type==BLUE_HIT:
                 blue_health-=1
-        if red_health<=0:
+        winner=""
+        if red_health<0:
             winner="BLUE WINS !!!"
-        if blue_health<=0:
+        if blue_health<0:
             winner="RED WINS !!!"
-
+        if winner != "":
+            show_winner(winner)
+            break;
 
         
         
@@ -120,8 +149,8 @@ def main():
         blue_movement(keys_pressed,blue)
         red_movement(keys_pressed,red)
         bullets_control(blue_bullets,red_bullets,blue,red)
-        draw_window(blue,red,red_bullets,blue_bullets)
-    pygame.quit()
+        draw_window(blue,red,red_bullets,blue_bullets,blue_health,red_health)
+    main()
 
 if __name__=="__main__":
     main()
